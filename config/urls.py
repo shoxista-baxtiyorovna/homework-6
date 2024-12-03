67f67052cc996058c82545376bbe1c62f5b5b134
Student <student@iMac-Astrum-30.local>
2024-12-03 10:01:48 +0500

    new changes

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from students.views import student_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('student/list/', student_list),
 ]
